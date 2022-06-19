5f42007e279b2da929f6d608f7168694be92c6eb
guilherme-lima4 <guilherme.lima.dinizt@gmail.com>
2022-06-18 21:17:04 -0300

    Deploy Heroku, alterando config Banco de Dados

--- conexaoLoginCliente.php
+++ conexaoLoginCliente.php
@@ -1,9 +1,9 @@
 <?php
 
-$usuario = 'id18915334_guilherme123';
-$senha = 'Gz[C)GZ0O9uhF9)1';
-$database = 'id18915334_market';
-$host = 'localhost';
+$usuario = 'bd12c9b932cc13';
+$senha = 'f0785053';
+$database = 'heroku_a02c0d302263f51';
+$host = 'us-cdbr-east-05.cleardb.net';
 $porta = '3306';
 
 $mysqli = new mysqli($host, $usuario, $senha, $database);
